1f74e1e8bf54e547035895cc9921c6d038c07fab
Le Vourc'h Quentin <quentin.lv04@gmail.com>
2026-02-18 15:25:50 +0100

    build(esp32) : add driver_UART header and c files
    add the start of files to use the UART module of the esp32, function init added

---
+++ code_esp32_WROOM_32E/driver_UART.h
@@ -0,0 +1,31 @@
+#ifndef DRIVER_UART_H
+#define DRIVER_UART_H
+
+/* Set the pins used for UART */
+#define UART2_Rx 16
+#define UART2_Tx 17
+
+/* set a default configuration that can be overiden */
+#ifndef UART2_config
+    #define UART2_config SERIAL_8N1
+#endif
+
+/* set a default baudrate that can be overiden */
+#ifndef UART2_baudrate
+    #define UART2_baudrate 9600
+#endif
+
+#if DEBUG == 1
+/* Communication with PC */
+    #define USE_SERIAL Serial
+    #define Serial_baudrate 115200
+#endif
+
+/*
+Initialise the UART pins
+*/
+void setup_UART(void);
+
+
+
+#endif
---
+++ code_esp32_WROOM_32E/driver_UART.ino
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <HardwareSerial.h>
+#include "driver_uart.h"
+
+HardwareSerial uart(2);
+
+void setup_UART(void)
+{
+    uart.begin(UART2_baudrate, UART2_config, UART2_Rx, UART2_Tx);
+
+    #if DEBUG == 1
+        Serial.begin(Serial_baudrate);
+    #endif
+}
