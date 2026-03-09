55436ad544427a084e123a435129448f5c78c4c9
Le Vourc'h Quentin <quentin.lv04@gmail.com>
2026-03-09 09:00:06 +0100

    modif(esp32) : forgot to include this file in the former commit

--- code_esp32_WROOM_32E/deep_sleep.ino
+++ code_esp32_WROOM_32E/deep_sleep.ino
@@ -2,10 +2,11 @@
 #include "driver_load_switch.hpp"
 #include "deep_sleep.hpp"
 
+#define MULT_NS_TO_S 1000000
 
 void start_deep_sleep(int time) //Temps en seconde
 {    
     switch_load(0);
-    esp_sleep_enable_timer_wakeup(time*1000000);
+    esp_sleep_enable_timer_wakeup(time * MULT_NS_TO_S);
     esp_deep_sleep_start();
 }
